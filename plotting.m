1b22281ca0d26a955f2da0ef96738d0d75b52d06
Chen-Kuan Liao <czl5899@psu.edu>
2023-12-01 16:15:00 -0500

    adding code for recall and precision

--- plotting.m
+++ plotting.m
@@ -60,11 +60,53 @@ for i = 1:12000
     image = testImages(:, :, :, i);
     label = testingLabel(i, 1);
     prediction = grp2idx(classify(net2, image));
-    % disp(label);
-    % disp(prediction);
 
     confusionMatrix(label+1, prediction) = confusionMatrix(label+1, prediction) + 1;
 end
 
 disp("Confusion Matrix");
 disp(confusionMatrix);
+
+%--------------------------------------------------------------------------
+% calculating the Precision
+for column = 1:10
+    TP = 0;
+    FP = 0;
+    for row = 1:10
+        if row == column
+            TP = confusionMatrix(row, column);
+        else
+            FP = FP + confusionMatrix(row, column);
+        end
+    end
+    precision = TP / (TP + FP);
+    fprintf("precision for %s: %.2f\n", labelNames{column}, precision);
+end
+
+% calculating the Recall
+for row= 1:10
+    TP = 0;
+    FN = 0;
+    for column = 1:10
+        if row == column
+            TP = confusionMatrix(row, column);
+        else
+            FN = FN + confusionMatrix(row, column);
+        end
+    end
+    recall = TP / (TP + FN);
+    fprintf("recall for %s: %.2f\n", labelNames{row}, recall);
+end
+
+% acurracy 
+miss = 0;
+for i = 1:10
+    for j = 1:10
+        if i ~= j
+            miss = miss + confusionMatrix(i, j);
+        end
+    end
+end
+
+fprintf("acurracy: %.2f\n", (12000-miss)/12000);
+
